438dfdadfa6e5fa8ab95de360e27c83ce1c30de1
Constantin Lisnic <constantin@lisnic.dev>
2024-12-11 10:18:51 +0000

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Constantin Lisnic on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
